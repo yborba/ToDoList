3353e077efe2617cbaaa92cc1b34b28160530e55
Yesus Borba <36797475+yborba@users.noreply.github.com>
2021-04-24 17:15:48 -0300

    Update TodoModel.java

--- src/main/java/com/projeto/todolist/entities/TodoModel.java
+++ src/main/java/com/projeto/todolist/entities/TodoModel.java
@@ -1,6 +1,3 @@
-
-//Entidade
-
 package com.projeto.todolist.entities;
 
 public class TodoModel {
@@ -31,35 +28,5 @@ public class TodoModel {
     
     public void setSubregion(String subregion) {
         this.subregion = subregion;
-    }
-
-    // private String userId;
-    // private int id;
-    // private String title;
-    // private boolean completed;
-    // public String getUserId() {
-    //     return userId;
-    // }
-    
-    // public void setUserId(String userId) {
-    //     this.userId = userId;
-    // }
-    // public int getId() {
-    //     return id;
-    // }
-    // public void setId(int id) {
-    //     this.id = id;
-    // }
-    // public String getTitle() {
-    //     return title;
-    // }
-    // public void setTitle(String title) {
-    //     this.title = title;
-    // }
-    // public boolean isCompleted() {
-    //     return completed;
-    // }
-    // public void setCompleted(boolean completed) {
-    //     this.completed = completed;
-    // }    
+    }  
 }
